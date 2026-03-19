63461f1b0bcee84ff19b728e85676be67d48d903
saisandeep1701 <saisandeepm12@gmail.com>
2026-03-19 18:37:36 -0500

    fix: tightly bound CORS rules restricting domains natively to React Vite servers

--- server/src/CommerceHub.Api/Program.cs
+++ server/src/CommerceHub.Api/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
